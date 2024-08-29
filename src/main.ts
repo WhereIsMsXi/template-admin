4e9655436126467330c69ffb0270f42e60f6a5f7
WhereIsMsXi <whereismsxi@gmail.com>
2024-08-29 16:39:31 +0800

    feat(custom): 配置 pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import { setupPinia } from './store';
+
+import '@where_is_mr_li/style/dist/index.scss';
+
 function boot() {
   const app = createApp(App);
+
+  setupPinia(app);
 
   app.mount('#app');
 }
