5f9cb3c200cb2c43aa63975769f16e33aaa4684b
WhereIsMsXi <whereismsxi@gmail.com>
2024-08-29 16:55:43 +0800

    feat(custom): 配置 element-plus 并自动导入

--- src/router/routes/static.ts
+++ src/router/routes/static.ts
@@ -1,3 +1,11 @@
 import type { RouteRecordRaw } from 'vue-router';
 
-export const staticRoutes: Array<RouteRecordRaw> = [];
+export const staticRoutes: Array<RouteRecordRaw> = [
+  {
+    path: '/login',
+    component: () => import('@/pages/login/LoginView.vue'),
+    meta: {
+      title: '登录',
+    },
+  },
+];
