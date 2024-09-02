6fce958df5ff8e156dc9958c034166060bcf6149
WhereIsMsXi <whereismsxi@gmail.com>
2024-09-02 11:19:22 +0800

    feat(custom): 获取动态路由数据

---
+++ src/router/routes/dynamic.ts
@@ -0,0 +1,18 @@
+import { mustArray } from '@where_is_mr_li/utils';
+import type { RouteRecordRaw } from 'vue-router';
+
+function sortRoutes(prev: any, next: any) {
+  return (prev.meta?.sort || 0) - (next.meta?.sort || 0);
+}
+
+const modules = import.meta.glob('../modules/**/*.ts', { eager: true });
+export const dynamicRoute: RouteRecordRaw[] = [];
+
+Object.keys(modules).forEach((key) => {
+  const content = (modules as any)[key].default;
+  const routes = mustArray(content);
+
+  dynamicRoute.push(...routes);
+});
+
+dynamicRoute.sort(sortRoutes);
